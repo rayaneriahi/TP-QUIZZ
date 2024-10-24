db312d382f00ef48b4fd918ff88ab75a84eadd32
Quentin Lavenir <quentin.lavenir@outlook.fr>
2024-10-24 16:46:48 +0200

    Modifications 1 podium des meilleurs scores

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>quizz</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
+
+<!-- 1 tableau des scores et 1 graphique en barre -->  
 <div>
   <canvas id="myChart"></canvas>
 </div>
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
   const ctx = document.getElementById('myChart');
@@ -18,21 +19,57 @@
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Géographie', 'Histoire', 'Mathématiques', 'Culture', 'Santé'],
+      labels: ['Géographie', 'Histoire', 'Mathématiques', 'Culture','Résultat Total'],
       datasets: [{
         label: 'Score Quentin',
-        data: [5, 10, 15, 20, 25],
-        borderWidth: 5
-      },
-      {
-        label: 'Score Rayane',
-        data: [30, 35, 40, 45, 50],
+        data: [5, 10, 15, 20, 50],
         borderWidth: 5
       },
       {
         label: 'Score Nicolas',
-        data: [55, 60, 65, 70, 75],
+        data: [45, 50, 55, 60, 210],
         borderWidth: 5
+      },
+      {
+        label: 'Score Rayane',
+        data: [25, 30, 35, 40, 130],    
+        borderWidth: 5
+      }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio	: true,
+    }
+  });
+</script>
+
+
+<!-- 1 podium des meilleurs scores -->  
+<div>
+  <canvas id="myPodium"></canvas>
+</div>
+
+<script>
+  const ctx1 = document.getElementById('myPodium');
+
+  new Chart(ctx1 ,{
+    type: 'bar',
+    data: {
+      labels: ['Podium classement'],
+      datasets: [{
+        label: 'Médaille Bronze Quentin',
+        data: [50],
+        borderWidth: 10
+      },
+      {
+        label: 'Médaille Or Nicolas',
+        data: [210],
+        borderWidth: 10
+      },
+      {
+        label: 'Médaille Argent Rayane',
+        data: [130],
+        borderWidth: 10
       },]
     },
     options: {
@@ -40,9 +77,7 @@
         maintainAspectRatio	: true,
     }
   });
-
 </script>
 
-</div>
 </body>
 </html>
