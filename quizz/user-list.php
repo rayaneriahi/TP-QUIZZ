3312f89320946d5d95a4e2b3386399db2e9de289
Quentin Lavenir <quentin.lavenir@outlook.fr>
2024-10-30 14:54:59 +0100

    Responsive sur tous les supports

--- quizz/user-list.php
+++ quizz/user-list.php
@@ -38,61 +38,53 @@ session_start();
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Quizz</title>
 </head>
-<body class="h-screen flex flex-col">
+<body class="min-h-screen w-full flex flex-col bg-cover bg-center" style="background-image: url('../images/paysage.jpg');">
 
-    <img src="../images/paysage.jpg" class=" absolute h-full w-full z-0">
+<header class="flex flex-col sm:flex-row justify-between py-3 z-10 pt-10">
+    <div class="pl-5 sm:pl-10 text-xl sm:text-3xl font-semibold text-white sm:flex sm:items-center space-y-2 sm:space-y-0 sm:space-x-10">
+        <span><?php echo $user["name"]; ?></span>
+        <span>Best score (<?php echo $user["max_score"]; ?>)</span>
+    </div>
 
-    <header class="  flex flex-row justify-between py-3 z-10 pt-10">
+    <div class="flex flex-row text-white font-semibold items-center pr-5 sm:pr-10 text-xl sm:text-3xl space-x-5">
+        <a href="http://tp-quizz.test/quizz/quizz-start.php">
+            <button id="btnQuizz" class="hover:text-gray-400">Quizz</button>
+        </a>
+        <a href="http://tp-quizz.test/authentification/log-in.php">
+            <button class="hover:text-gray-400">Log out</button>
+        </a>
+    </div>
+</header>
 
-        <div class=" pl-10 text-3xl font-semibold text-white space-x-10">
+    <div class="grow flex flex-col z-10 justify-center items-center space-y-4 sm:space-y-8 md:space-y-10 p-3 sm:p-4 md:p-6">
 
-            <span><?php echo $user["name"];?></span>
+    <div class="min-h-1/2 h-auto sm:h-1/2 md:h-2/3 w-full sm:w-10/12 md:w-8/12 bg-white rounded-2xl p-3 sm:p-4 md:p-6 flex flex-col">
 
-            <span>Best score (<?php echo $user["max_score"]?>)</span>
+        <h1 class="text-blue-800 text-xl sm:text-3xl md:text-4xl font-semibold text-center">Leader board</h1>
 
-        </div>
-
-        <div class=" flex flex-row text-white font-semibold items-center pr-10 text-3xl space-x-10">
-            
-            <a href="http://tp-quizz.test/quizz/quizz-start.php"><button id="btnQuizz" class=" hover:text-gray-400">Quizz</button></a>
-
-            <a href="http://tp-quizz.test/authentification/log-in.php"> <button class=" hover:text-gray-400">Log out</button> </a>
-
-        </div>
-
-    </header>
-
-    <div class="grow flex flex-col z-10 justify-center items-center space-y-10">
-
-        <div class="h-1/2 w-10/12 bg-white rounded-2xl p-5 flex flex-col">
-
-            <h1 class=" text-blue-800 text-4xl font-semibold place-self-center sticky">Leader board</h1>
-            
-            <div class="grow">            
-                
-                <canvas id="myChart"></canvas>
-
-            </div>
-
-        </div>
-
-        <div class="bg-white w-10/12 h-1/3 space-y-5 rounded-2xl p-5">
-
-            <h1 class=" text-blue-800 text-4xl font-semibold place-self-center sticky">Users list</h1>
-
-            <div class="flex flex-row space-x-5">
-
-                <?php foreach ($users as $value) { ?>
-
-                            <p class ="text-2xl w-1/5"><?php echo $value["name"]; ?></p>
-
-                <?php } ?>
-
-            </div>
-
+        <div class="grow overflow-y-auto">            
+            <canvas id="myChart"></canvas>
         </div>
 
     </div>
+
+    <div class="bg-white w-full sm:w-10/12 md:w-8/12 h-auto sm:h-1/3 md:h-1/3 space-y-3 sm:space-y-5 md:space-y-6 rounded-2xl p-3 sm:p-5 md:p-6">
+
+        <h1 class="text-blue-800 text-xl sm:text-3xl md:text-4xl font-semibold text-center">Users list</h1>
+
+        <div class="flex flex-col sm:flex-row md:grid md:grid-cols-2 gap-3 sm:gap-5 overflow-y-auto max-h-40">
+            <?php foreach ($users as $value) { ?>
+                <p class="text-base sm:text-xl md:text-2xl w-full md:w-auto text-center sm:text-left">
+                    <?php echo $value["name"]; ?>
+                </p>
+            <?php } ?>
+        </div>
+        
+    </div>
+
+</div>
+
+
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
